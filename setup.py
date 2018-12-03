bc2a3601192e8bb41a6acae0bd20e70c74141736
Guilherme Cano Lopes <gui.c.lopes@gmail.com>
2018-12-03 17:40:02 -0200

    PypeCast initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pypecast',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Pypecast is a python package for preprocessing, evaluating and forecasting general time series with Artificial Neural Networks (ANN).',
+    author='Guilherme Cano Lopes',
+    license='MIT',
+)
