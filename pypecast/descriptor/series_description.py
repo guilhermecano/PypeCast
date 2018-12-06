5705a5e5e9c5abdc5702cec9483c0acb88f465f1
Guilherme Cano Lopes <gui.c.lopes@gmail.com>
2018-12-06 20:16:53 -0200

    Minor changes in descriptor, autocorrelation added.

--- pypecast/descriptor/series_description.py
+++ pypecast/descriptor/series_description.py
@@ -3,38 +3,70 @@ from __future__ import print_function
 from __future__ import division
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from statsmodels.tsa.stattools import acf, acovf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import warnings
 
 class SeriesDescriptor(object):
+    '''Class for quickly analyzing a time series. '''
+
     def __init__(self):
         pass
     
     def full_report(self, data):
-        raise(NotImplementedError)
+        self.describe(data)
+        self.autocorr(data)
 
     def describe(self, data):
-        print('Description of the series data:')
+        print('-> Description of the series data:')
         if isinstance(data,(pd.DataFrame,)):
             print(data.describe())
         else:
             print(pd.DataFrame(data).describe()) 
 
-    def autocorrelation(self, data):
-        raise(NotImplementedError)
+    def autocorr(self, data, unbiased = False, ):
 
+        #configuration of the plot
+        print('-> Autocorrelation and partial autocorrelation:')
+        warnings.filterwarnings("ignore")
+        sns.set()
+        plt.figure(figsize=[12,6])
+
+        #Autocorrelation array
+        ac = acf(data)
+        
+        plot_acf(data)
+
+        plt.title('Autocorrelation of the series')
+        plt.xlabel('timestep')
+        plt.ylabel('autocorrelation')
+
+        plot_pacf(data)
+
+        plt.title('Partial autocorrelation of the series')
+        plt.xlabel('timestep')
+        plt.ylabel('autocorrelation')
+        sns.reset_defaults()
+        return ac
+        
     def trend_check(self, data):
-        raise(NotImplementedError)
+        print('-> Checking for trends:')
     
     def seasonality_check(self, data):
-        raise(NotImplementedError)
+        print('-> Checking for seasonality:')
     
     def outliers_check(self, data):
-        raise(NotImplementedError)
+        print('-> Checking for outliers:')
 
     def lr_cycle_check(self, data):
-        raise(NotImplementedError)
+        print('-> Checking for long-run cycles:')
     
     def const_var_check(self, data):
-        raise(NotImplementedError)
+        print('-> Checking if series variance is constant:')
 
     def get_abrupt_changes(self, data):
-        raise(NotImplementedError)
+        print('-> Checking for abrupt changes:')
+        
